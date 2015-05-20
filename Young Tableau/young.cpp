a762fd58be7fe5fee215a545a0813f1a537d7874
diogomartins <diogo.martins@unirio.br>
2015-05-20 13:56:32 -0300

    Refatoração

--- Young Tableau/young.cpp
+++ Young Tableau/young.cpp
@@ -7,11 +7,11 @@
 //
 
 #include "young.h"
-
+#define INFINITO INT_MAX
 /**
  1. O construtor cria um quadro vazio. Para tal, aloca espaço 
  para a matriz de inteiros Y, m × n e inicializa os valores com 
- a constante simb ́olica INT_MAX para representar ∞.
+ a constante simbólica INT_MAX para representar ∞.
  */
 young::young(int linhas, int colunas){
     m = linhas;
@@ -19,7 +19,7 @@ young::young(int linhas, int colunas){
     
     for (int i=0; i<=m; i++)
         for(int j=0; i<=n; i++)
-            Y[i][j] = INT_MAX;
+            Y[i][j] = INFINITO;
 }
 
 /**
@@ -36,7 +36,7 @@ young::~young(){
 bool young::vazio(){
     for (int i=0; i<=m; i++)
         for(int j=0; i<=n; i++)
-            if (Y[i][j] != INT_MAX)
+            if (Y[i][j] != INFINITO)
                 return false;       // Se achou pelo menos uma posição preenchida
     return true;                    // Não achou nenhuma posição preenchida
 }
@@ -47,15 +47,20 @@ bool young::vazio(){
 bool young::cheio(){
     for (int i=0; i<=m; i++)
         for(int j=0; i<=n; i++)
-            if (Y[i][j] == INT_MAX)
+            if (Y[i][j] == INFINITO)
                 return false;       // Se achou pelo menos uma posição vazia
     return true;                    // Não achou nenhuma posição vazia
 }
 
+void young::youngify(int i, int j){
+    
+}
 /**
 5. Função bool remove(int & elem) para extrair o menor elemento do quadro. Uma vez que o menor elemento está sempre na posição Y [1, 1] (no caso da linguagem C, Y [0, 0] ) a função pode retornar o elemento armazenado em Y [1, 1] e fazer Y [1, 1] = ∞ para indicar que o elemento não existe mais. Entretanto, tal fato pode deixar o quadro inconsistente. Isso acontece quando Y [1, 1] = ∞ e o quadro não está vazio.
 */
 bool young::remove(int &elem){
+    elem = Y[0][0];
+    Y[0][0] = INFINITO;
     return false;
 }
 
@@ -63,5 +68,10 @@ bool young::remove(int &elem){
 6. Função bool insere(int valor), para inserir um elemento no quadro. Se o quadro não estiver cheio, então Y [m, n] = ∞ (no caso da linguagem C, Y [m − 1, n − 1] ) e é possível colocar o novo elemento nesta posição. Entretanto, o quadro pode ficar inconsistente. Pode-se resolver este problema recursivamente retraindo o elemento novo e trocando-o com o elemento da esquerda ou do topo até conseguir um quadro de Young. A função retorna true se o elemento foi inserido com sucesso e false, caso contrário.
 */
 bool young::insere(int valor){
+    if (!cheio()){
+        Y[m-1][n-1] = valor;
+        // Younnify
+        return true;
+    }
     return false;
 }
