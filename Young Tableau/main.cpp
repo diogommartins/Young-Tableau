a7c48860d62819debc6f1b462f73686dfdf23cf0
diogomartins <diogo.martins@unirio.br>
2015-05-20 16:33:36 -0300

    Adicionado menu e loop de execução da main

--- Young Tableau/main.cpp
+++ Young Tableau/main.cpp
@@ -7,9 +7,44 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+#include "young.h"
+
+using namespace std;
+
+int Menu(){
+    int escolha;
+    
+    cout << "--------------------------\n";
+    cout << "Selecione uma opcao:\n";
+    cout << "1. Insere elemento (no. inteiro) no Quadro de Young.\n";
+    cout << "2. Remove menor elemento do Quadro de Young.\n";
+    cout << "3. Imprime elementos do Quadro de Young.\n";
+    cout << "0. Fim.\n";
+    cout << "Sua opcao: ";
+    cin >> escolha;
+    return escolha;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int item, escolha;
+    bool fim = false;
+    
+    while ((!fim) && (escolha = Menu())){
+        switch (escolha) {
+            case 0:
+                fim = true;
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
+    
     return 0;
 }
