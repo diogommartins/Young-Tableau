34742bddeba494dae080a46b25acb61f7f9789eb
diogomartins <diogo.martins@unirio.br>
2015-05-20 12:40:29 -0300

    Adicionado construtor de young

--- Young Tableau/young.cpp
+++ Young Tableau/young.cpp
@@ -7,3 +7,18 @@
 //
 
 #include "young.h"
+
+/**
+ 1. O construtor cria um quadro vazio. Para tal, aloca espaço 
+ para a matriz de inteiros Y, m × n e inicializa os valores com 
+ a constante simb ́olica INT_MAX para representar ∞.
+ */
+young::young(int linhas, int colunas){
+    m = linhas;
+    n = colunas;
+    
+    for (int i=0; i<=m; i++)
+        for(int j=0; i<=n; i++)
+            Y[i][j] = INT_MAX;
+}
+
