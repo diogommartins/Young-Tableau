b33e9dba2c5ea0126cb5ee079559e0452654b1d7
diogomartins <diogo.martins@unirio.br>
2015-05-20 12:55:52 -0300

    Adicionada implementação de young::vazio()

--- Young Tableau/young.cpp
+++ Young Tableau/young.cpp
@@ -33,3 +33,31 @@ young::~young(){
  3. Função bool vazio() retorna true se o quadro está vazio e false,
  caso contrário.
 */
+bool young::vazio(){
+    for (int i=0; i<=m; i++)
+        for(int j=0; i<=n; i++)
+            if (Y[i][j] != INT_MAX)
+                return false;
+    return true;
+}
+
+/**
+4. Função bool cheio() retorna true se o quadro está cheio e false, caso contrário.
+*/
+bool young::cheio(){
+    return false;
+}
+
+/**
+5. Função bool remove(int & elem) para extrair o menor elemento do quadro. Uma vez que o menor elemento está sempre na posição Y [1, 1] (no caso da linguagem C, Y [0, 0] ) a função pode retornar o elemento armazenado em Y [1, 1] e fazer Y [1, 1] = ∞ para indicar que o elemento não existe mais. Entretanto, tal fato pode deixar o quadro inconsistente. Isso acontece quando Y [1, 1] = ∞ e o quadro não está vazio.
+*/
+bool young::remove(int &elem){
+    return false;
+}
+
+/**
+6. Função bool insere(int valor), para inserir um elemento no quadro. Se o quadro não estiver cheio, então Y [m, n] = ∞ (no caso da linguagem C, Y [m − 1, n − 1] ) e é possível colocar o novo elemento nesta posição. Entretanto, o quadro pode ficar inconsistente. Pode-se resolver este problema recursivamente retraindo o elemento novo e trocando-o com o elemento da esquerda ou do topo até conseguir um quadro de Young. A função retorna true se o elemento foi inserido com sucesso e false, caso contrário.
+*/
+bool young::insere(int valor){
+    return false;
+}
